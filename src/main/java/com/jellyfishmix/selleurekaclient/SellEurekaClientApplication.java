6075a710bc536fd5328bfc4b10c441a12727130b
JellyfishMIX <2273516339@qq.com>
2020-05-19 16:19:20 +0800

    :tada: Initial commit

---
+++ src/main/java/com/jellyfishmix/selleurekaclient/SellEurekaClientApplication.java
@@ -0,0 +1,13 @@
+package com.jellyfishmix.selleurekaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellEurekaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SellEurekaClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jellyfishmix/selleurekaclient/SellEurekaClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jellyfishmix.selleurekaclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellEurekaClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
